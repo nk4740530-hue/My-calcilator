720c897d8d40d2f644e701be49a247ae5285ffee
nk4740530-hue <nk4740530@gmail.com>
2026-08-05 19:54:40 +0530

    Refactor calculator layout to use BoxLayout

--- calculator.py
+++ calculator.py
@@ -1,5 +1,5 @@
 from kivy.app import App
-from kivy.uix.gridlayout import GridLayout
+from kivy.uix.boxlayout import BoxLayout
 from kivy.uix.button import Button
 from kivy.uix.textinput import TextInput
 
@@ -7,49 +7,63 @@ from kivy.uix.textinput import TextInput
 class Calculator(App):
 
     def build(self):
-        layout = GridLayout(cols=4)
+        layout = BoxLayout(orientation="vertical")
 
         self.display = TextInput(
             text="",
             readonly=True,
-            font_size=35,
-            halign="right"
+            halign="right",
+            font_size=40
         )
-
         layout.add_widget(self.display)
 
         buttons = [
-            "7", "8", "9", "/",
-            "4", "5", "6", "*",
-            "1", "2", "3", "-",
-            "C", "0", "=", "+"
+            ["C", "⌫", "/", "*"],
+            ["7", "8", "9", "-"],
+            ["4", "5", "6", "+"],
+            ["1", "2", "3", "="],
+            ["0", ".", "%"]
         ]
 
-        for text in buttons:
-            button = Button(
-                text=text,
-                font_size=30
-            )
-            button.bind(on_press=self.button_pressed)
-            layout.add_widget(button)
+        for row in buttons:
+            row_layout = BoxLayout()
+
+            for value in row:
+                button = Button(
+                    text=value,
+                    font_size=28
+                )
+                button.bind(on_press=self.click)
+                row_layout.add_widget(button)
+
+            layout.add_widget(row_layout)
 
         return layout
 
-    def button_pressed(self, instance):
-        value = instance.text
+    def click(self, button):
+        value = button.text
 
         if value == "C":
             self.display.text = ""
 
+        elif value == "⌫":
+            self.display.text = self.display.text[:-1]
+
         elif value == "=":
             try:
-                self.display.text = str(
-                    eval(self.display.text)
-                )
+                self.display.text = str(eval(self.display.text))
+            except:
+                self.display.text = "Error"
+
+        elif value == "%":
+            try:
+                self.display.text = str(float(self.display.text) / 100)
             except:
                 self.display.text = "Error"
 
         else:
+            if self.display.text == "Error":
+                self.display.text = ""
             self.display.text += value
 
 
